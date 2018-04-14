85fc541a35b19260d66235efbc067203d751cffa
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-04-14 08:41:15 -0300

    Criado classe para enviar email Smtp

---
+++ src/main/java/com/andersonmarques/cursomc/services/SmtpEmailService.java
@@ -0,0 +1,26 @@
+package com.andersonmarques.cursomc.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+
+public class SmtpEmailService extends AbstractEmailService {
+	
+	//Não fiz a configuração para enviar o email com o gmail no properties
+	
+	@Autowired
+	private EmailService emailService;
+	
+	private static final Logger log = LoggerFactory.getLogger(SmtpEmailService.class);
+	
+	@Override
+	public void sandEmail(SimpleMailMessage email) {
+		log.info("Simulando envio de email");
+//		emailService.sandEmail(email);
+		log.info("Email enviado");
+	}
+	
+	
+
+}
