80888f692850f82730265f8edb24f88d57a7416c
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-04-14 20:02:44 -0300

    Criado token refresh

---
+++ src/main/java/com/andersonmarques/cursomc/resources/AuthResources.java
@@ -0,0 +1,29 @@
+package com.andersonmarques.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andersonmarques.cursomc.security.JWTUtil;
+import com.andersonmarques.cursomc.security.UserSS;
+import com.andersonmarques.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResources {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
